2ddd87553bd52bce514be84770ae98b1c11de0dd
Martin Florian <martin.florian@hu-berlin.de>
2019-08-03 08:16:00 +0200

    Basic data types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+use bit_set::BitSet;
+
+type NodeID = usize; // internal and possibly different between runs
+type PublicKey = String;
+
+struct Network {
+    nodes: Vec<Node>,
+}
+struct Node {
+    pk: PublicKey,
+    qset: QuorumSet,
+}
+struct QuorumSet {
+    threshold: usize,
+    validators: BitSet<NodeID>,
+    inner_quorum_sets: Vec<QuorumSet>
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
