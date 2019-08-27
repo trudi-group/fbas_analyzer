91c6063bc029db38bc1dd12976c3fc4d5d114b5f
Martin Florian <martin.florian@hu-berlin.de>
2019-08-27 21:54:15 +0200

    Some flags to control what to output / compute

--- src/main.rs
+++ src/main.rs
@@ -5,48 +5,79 @@ use fbas_analyzer::*;
 use quicli::prelude::*;
 use structopt::StructOpt;
 
+/// Learn things about a given FBAS
 #[derive(Debug, StructOpt)]
 struct Cli {
-    /// Path to JSON file describing the FBAS in stellarbeat.org format.
+    /// Path to JSON file describing the FBAS in stellarbeat.org format
     path: String,
+
+    /// Output (and find) minimal quorums
+    #[structopt(short = "q", long = "minimal-quorums")]
+    minimal_quorums: bool,
+
+    /// Output (and find) minimal blocking sets
+    #[structopt(short = "b", long = "minimal-blocking-sets")]
+    minimal_blocking_sets: bool,
+
+    /// Check for quorum intersection, output result
+    #[structopt(short = "i", long = "quorum-intersection")]
+    quorum_intersection: bool,
 
     #[structopt(flatten)]
     verbosity: Verbosity,
 }
 
 fn main() -> CliResult {
-
     let args = Cli::from_args();
     args.verbosity.setup_env_logger("fbas_analyzer")?;
 
     let network = Network::from_json_file(&args.path);
 
-    println!(
-        "(In all following dumps, nodes are identified by their index in the input JSON.)\n"
+    let (q, b, i) = (
+        args.minimal_quorums,
+        args.minimal_blocking_sets,
+        args.quorum_intersection,
     );
-
-    let minimal_quorums = get_minimal_quorums(&network);
-    println!("We found {} minimal quorums:", minimal_quorums.len());
-    println!("\n{}\n", node_sets_to_json(&minimal_quorums));
-
-    let minimal_blocking_sets = get_minimal_blocking_sets(&minimal_quorums);
-    println!(
-        "We found {} minimal blocking sets:",
-        minimal_blocking_sets.len()
-    );
-    println!("\n{}\n", node_sets_to_json(&minimal_blocking_sets));
-
-    println!(
-        "Control over any of these node sets is sufficient to compromise liveliness and \
-         censor future transactions.\n"
-    );
-
-    if all_node_sets_interesect(&minimal_quorums) {
-        println!("All quorums intersect.");
+    // no flags set => output everything
+    let (q, b, i) = if (q, b, i) == (false, false, false) {
+        (true, true, true)
     } else {
-        println!("Some quorums don't intersect - safety severely threatened for some nodes!");
-    }
-    println!();
+        (q, b, i)
+    };
 
+    if q || b {
+        println!(
+            "(In all following dumps, nodes are identified by their index in the input JSON.)\n"
+        );
+    }
+    if q || b || i {
+        let minimal_quorums = get_minimal_quorums(&network);
+
+        if q {
+            println!("We found {} minimal quorums:", minimal_quorums.len());
+            println!("\n{}\n", node_sets_to_json(&minimal_quorums));
+        }
+        if b {
+            let minimal_blocking_sets = get_minimal_blocking_sets(&minimal_quorums);
+            println!(
+                "We found {} minimal blocking sets:",
+                minimal_blocking_sets.len()
+            );
+            println!("\n{}\n", node_sets_to_json(&minimal_blocking_sets));
+            println!(
+                "Control over any of these node sets is sufficient to compromise liveliness and \
+                 censor future transactions.\n"
+            );
+        }
+        if i {
+            if all_node_sets_interesect(&minimal_quorums) {
+                println!("All quorums intersect.");
+            } else {
+                println!(
+                    "Some quorums don't intersect - safety severely threatened for some nodes!"
+                );
+            }
+        }
+    }
     Ok(())
 }
