c276b20d0d829f91c5e69305ab79a3397ebc104e
Martin Florian <martin.florian@hu-berlin.de>
2019-10-09 16:44:28 +0200

    Benchmark update & fix

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,26 +5,54 @@ extern crate fbas_analyzer;
 use criterion::black_box;
 use criterion::Criterion;
 
-use fbas_analyzer::{all_interesect, find_minimal_blocking_sets, find_minimal_quorums, Fbas};
+use std::path::Path;
+
+use fbas_analyzer::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let fbas = Fbas::from_json_file("test_data/stellarbeat_nodes_2019-09-17.json");
-    let minimal_quorums = find_minimal_quorums(&fbas);
+    let fbas = Fbas::from_json_file(Path::new("test_data/stellarbeat_nodes_2019-09-17.json"));
+    let orgs = Organizations::from_json_file(
+        Path::new("test_data/stellarbeat_organizations_2019-09-17.json"),
+        &fbas,
+    );
 
     c.bench_function("find_minimal_quorums", |b| {
         b.iter(|| find_minimal_quorums(black_box(&fbas)))
     });
+    let minimal_quorums = find_minimal_quorums(&fbas);
+
     c.bench_function("all_interesect", |b| {
         b.iter(|| all_interesect(black_box(&minimal_quorums)))
     });
+
+    c.bench_function("collapse_by_organization", |b| {
+        b.iter(|| {
+            remove_non_minimal_node_sets(
+                orgs.collapse_node_sets(black_box(minimal_quorums.clone())),
+            )
+        })
+    });
+    let minimal_quorums_collapsed =
+        remove_non_minimal_node_sets(orgs.collapse_node_sets(minimal_quorums.clone()));
+
+    c.bench_function("find_minimal_blocking_sets_collapsed", |b| {
+        b.iter(|| find_minimal_blocking_sets(black_box(&minimal_quorums_collapsed)))
+    });
+    c.bench_function("find_minimal_intersections_collapsed", |b| {
+        b.iter(|| find_minimal_intersections(black_box(&minimal_quorums_collapsed)))
+    });
+
     c.bench_function("find_minimal_blocking_sets", |b| {
         b.iter(|| find_minimal_blocking_sets(black_box(&minimal_quorums)))
+    });
+    c.bench_function("find_minimal_intersections", |b| {
+        b.iter(|| find_minimal_intersections(black_box(&minimal_quorums)))
     });
 }
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().sample_size(20);
+    config = Criterion::default().sample_size(10);
     targets = criterion_benchmark
 }
 criterion_main!(benches);
