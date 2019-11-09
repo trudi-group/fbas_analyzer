a6f7259385d2b8a6015466617d49bc35ec51fb2d
Martin Florian <martin.florian@hu-berlin.de>
2019-11-09 15:39:32 +0100

    Performance improvements

--- src/analyses/find_intersections.rs
+++ src/analyses/find_intersections.rs
@@ -1,7 +1,40 @@
 use super::*;
 
 pub fn find_minimal_intersections(node_sets: &[NodeIdSet]) -> Vec<NodeIdSet> {
-    find_optionally_smallest_minimal_intersections(node_sets, None)
+    // TODO clean me up
+    // find_optionally_smallest_minimal_intersections(node_sets, None)
+
+    let max_len_upper_bound = node_sets.iter().map(|x| x.len()).max().unwrap_or(0);
+    let mut buckets_by_len: Vec<Vec<(usize, usize)>> = vec![vec![]; max_len_upper_bound];
+
+    // we don't store intersections themselves at this stage to save memory
+    // (this also improves cache locality during second step?)
+    let mut intersection;
+    for (i, ns1) in node_sets.iter().enumerate() {
+        for (j, ns2) in node_sets.iter().enumerate().skip(i + 1) {
+            intersection = ns1.clone();
+            intersection.intersect_with(ns2);
+            buckets_by_len[intersection.len()].push((i, j));
+        }
+    }
+
+    // second step: reduce to minimal
+    let mut minimal_intersections: Vec<NodeIdSet> = vec![];
+    let mut minimal_intersections_current_len: Vec<NodeIdSet> = vec![];
+    for bucket in buckets_by_len.into_iter() {
+        for (i, j) in bucket.into_iter() {
+            intersection = node_sets[i].clone();
+            intersection.intersect_with(&node_sets[j]);
+            if minimal_intersections
+                .iter()
+                .all(|x| !x.is_subset(&intersection))
+            {
+                minimal_intersections.push(intersection);
+            }
+        }
+        minimal_intersections.append(&mut minimal_intersections_current_len);
+    }
+    minimal_intersections
 }
 
 pub fn find_smallest_minimal_intersections(
@@ -46,15 +79,17 @@ pub fn find_optionally_smallest_minimal_intersections(
     minimal_intersections
 }
 
+// TODO never used anymore, clean me up
 fn find_intersections(node_sets: &[NodeIdSet]) -> Vec<NodeIdSet> {
     let expected_len = node_sets.len() * (node_sets.len() - 1) / 2;
     let mut intersections: Vec<NodeIdSet> = Vec::with_capacity(expected_len);
 
+    let mut intersection; // defining this here saves allocations...
     for (i, ns1) in node_sets.iter().enumerate() {
         for ns2 in node_sets.iter().skip(i + 1) {
-            let mut intersection = ns1.clone();
+            intersection = ns1.clone();
             intersection.intersect_with(ns2);
-            intersections.push(intersection);
+            intersections.push(intersection.clone());
         }
     }
     intersections
