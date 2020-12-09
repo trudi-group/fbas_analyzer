a91bb4a52e50f605c3dfadc66d745b96c6758862
Martin Florian <martin.florian@hu-berlin.de>
2020-12-09 12:34:55 +0100

    v0.5.0
    This version introduces major API and CLI changes related to node
    merging (by organization, and now also by ISP and country).

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@
 //!
 //! ...can be found in the `src/bin` and `examples` folders...
 
-#![doc(html_root_url = "https://docs.rs/fbas_analyzer/0.4.3")]
+#![doc(html_root_url = "https://docs.rs/fbas_analyzer/0.5.0")]
 
 mod analysis;
 mod core_types;
