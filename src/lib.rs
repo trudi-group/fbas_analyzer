c720e2b5cb1e1aa2959b205ac27faa7d82265b69
Martin Florian <martin.florian@hu-berlin.de>
2021-05-07 11:28:36 +0200

    v0.5.4

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@
 //!
 //! ...can be found in the `src/bin` and `examples` folders...
 
-#![doc(html_root_url = "https://docs.rs/fbas_analyzer/0.5.3")]
+#![doc(html_root_url = "https://docs.rs/fbas_analyzer/0.5.4")]
 
 mod analysis;
 mod core_types;
